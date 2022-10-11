4bd915a97c4c636bbecafed3596b15ab3b6b0960
Alex <alexandre.dutay@outlook.fr>
2022-10-11 23:04:34 +0200

    csl log to debug cart order

--- components/cart/cartTotal/index.js
+++ components/cart/cartTotal/index.js
@@ -57,7 +57,9 @@ export const CartTotal = ({}) => {
                     bearer: jwt
                 })
                 setSubmitError(null)
+                console.log('statusText', statusText)
                 if (statusText) {
+                    console.log('setIsModalVisible(true)')
                     setIsModalVisible(true)
                     
                 }
